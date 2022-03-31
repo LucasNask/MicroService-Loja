3d233dfbf66f2557123d945aae39e00e7d698765
Lucas Nascimento <Lucas_nascimento1@hotmail.com>
2022-03-31 18:01:27 -0300

    Comunicação do microservice do fornecedor

--- src/main/java/br/com/alura/microservices/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservices/loja/service/CompraService.java
@@ -2,6 +2,7 @@ package br.com.alura.microservices.loja.service;
 
 import br.com.alura.microservices.loja.dto.CompraDTO;
 import br.com.alura.microservices.loja.dto.InfoFornecedorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,21 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CompraService {
 
+    @Autowired
+    private RestTemplate client;
+
     public void realizaCompra(CompraDTO compraDTO) {
 
-        RestTemplate client = new RestTemplate();
+      /*  RestTemplate client = new RestTemplate();
         ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://localhost:8081/info/" + compraDTO.getEndereco().getEstado(),
                 HttpMethod.GET, null, InfoFornecedorDTO.class);
+
+        System.out.printf(exchange.getBody().getEndereco()); */
+
+        ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://Fornecedor/info/" + compraDTO.getEndereco().getEstado(),
+                HttpMethod.GET, null, InfoFornecedorDTO.class);
+
+        System.out.printf(exchange.getBody().getEndereco());
 
     }
 }
